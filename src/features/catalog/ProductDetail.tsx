72bb8841de467687911fd563958cafa1a46842d6
anurak789 <anurak.net@gmail.com>
2025-05-29 08:09:14 +0700

    Update the add and remove item functions

--- src/features/catalog/ProductDetail.tsx
+++ src/features/catalog/ProductDetail.tsx
@@ -5,9 +5,22 @@ import Grid from "@mui/material/Grid2";
 import { Button, Divider, TableBody, TableCell, TableContainer, TableRow, TextField, Typography } from "@mui/material";
 //import { Details } from "@mui/icons-material";
 import { useFetchProductDetailsQuery } from "./catalogApi";
+import { useAddBasketItemMutation, useFetchBasketQuery, useRemoveBasketItemMutation } from "../Basket/basketApi";
+import { ChangeEvent, useEffect, useState } from "react";
 
 export default function ProductDetail() {
   const {id} = useParams();
+  const [removeBasketItem] = useRemoveBasketItemMutation();
+  const [addBasketItem] = useAddBasketItemMutation();
+  const {data: basket} = useFetchBasketQuery();
+  const item = basket?.items.find(x => x.productId === +id!);
+  const [quantity, setQuantity] = useState(0);
+
+  useEffect(() => {
+    if (item) setQuantity(item.quantity);
+  }, [item]);
+
+
 //   const [product, setProduct] = useState<Product | null>(null);
 
 //   useEffect(() => {
@@ -20,6 +33,22 @@ export default function ProductDetail() {
   const {data: product, isLoading} = useFetchProductDetailsQuery(id ? +id : 0)
   
   if(!product || isLoading) return <div>Loading...</div>
+
+  const handleUpdateBasket = () => {
+    const updatedQuantity = item ? Math.abs(quantity - item.quantity) : quantity
+
+    if (!item || quantity > item.quantity) {
+      addBasketItem({product, quantity: updatedQuantity})
+    } else {
+      removeBasketItem({productId: product.id, quantity: updatedQuantity})
+    }
+  }
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = +event.currentTarget.value;
+
+    if (value >= 0) setQuantity(value)
+  }
 
   const productDetails = [
     {label: 'Name', value: product.name},
@@ -61,11 +90,20 @@ export default function ProductDetail() {
                         type="number"
                         label='Quantity in basket'
                         fullWidth
-                        defaultValue={1}
+                        value={quantity}
+                        onChange={handleInputChange}
                     />
                 </Grid>
                 <Grid>
-                    <Button sx={{height: '55px'}} color="primary" size="large" variant="contained" fullWidth>
+                    <Button
+                      onClick={handleUpdateBasket} 
+                      disabled={quantity === item?.quantity || !item && quantity === 0}
+                      sx={{height: '55px'}} 
+                      color="primary" 
+                      size="large" 
+                      variant="contained" 
+                      fullWidth
+                    >
                         Add to cart
                     </Button>
                 </Grid>
